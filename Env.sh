2c62ff104b080b9e6c5001865f7867ac82858022
Tomasz Dziopa <td366732@students.mimuw.edu.pl>
2016-01-05 21:11:46 +0100

    No Env.sh

--- Env.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-BASEDIR="/home/tomek/dev/u2/sr/cloudalbum"
-CLOUDALBUM_JAVA_OPTS="-Djava.security.debug=access,failure"
-CLOUDALBUM_DEBUG_OPTS="-Djava.rmi.server.logCalls=true \
-    -Dsun.rmi.server.logLevel=VERBOSE \
-    -Dsun.rmi.client.logCalls=true \
-    -Dsun.rmi.transport.tcp.logLevel=VERBOSE"
