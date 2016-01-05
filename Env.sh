cc7ff01a05c0c4592e4e6e3f115488d55f5a6fcf
Tomasz Dziopa <td366732@students.mimuw.edu.pl>
2016-01-05 21:11:28 +0100

    paths

--- Env.sh
+++ Env.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+BASEDIR="/home/tomek/dev/u2/sr/cloudalbum"
 CLOUDALBUM_JAVA_OPTS="-Djava.security.debug=access,failure"
 CLOUDALBUM_DEBUG_OPTS="-Djava.rmi.server.logCalls=true \
     -Dsun.rmi.server.logLevel=VERBOSE \
--- FetcherServer.sh
+++ FetcherServer.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 source Env.sh
-java -classpath /home/tomek/dev/u2/sr/cloudalbum/out/production/cloudalbum/ \
+java -classpath $BASEDIR/out/production/cloudalbum/ \
     $CLOUDALBUM_JAVA_OPTS \
     $CLOUDALBUM_DEBUG_OPTS \
     -Djava.rmi.server.hostname=localhost \
-    -Djava.rmi.server.codebase=file:/home/tomek/dev/u2/sr/cloudalbum/out/production/cloudalbum/ \
+    -Djava.rmi.server.codebase=file:$BASEDIR/out/production/cloudalbum/ \
     -Djava.security.policy=FetcherServer.policy \
 	  pl.edu.mimuw.cloudalbum.fetcher.FetcherModule
