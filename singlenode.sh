2dc65deff32d84c0dca6401f782dc542d0ace0d5
tomecki <tomek.dziopa@gmail.com>
2016-01-06 20:58:49 +0100

    delay

--- singlenode.sh
+++ singlenode.sh
@@ -7,6 +7,7 @@ tmux new -s c -d
 tmux split-window -h -t c:0.0
 tmux split-window -h -t c:0.1
 tmux send-keys -t c:0.0 './FetcherServer.sh' C-m
+sleep 2
 tmux send-keys -t c:0.1 './FetcherClient.sh' C-m
 tmux send-keys -t c:0.2 'ls' C-m
 
--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@ cd sr/cloudalbum
 git pull
 ant
 
-hosts=(violet04 yellow01)
-for nodehost in $hosts 
+hosts="violet04 yellow01"
+for nodehost in $hosts
 do
     ssh "td366732@"$nodehost < singlenode.sh
 done
