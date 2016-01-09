affb2c958b7be1abc4355eaf6205d6e6540d101c
tomecki <tomek.dziopa@gmail.com>
2016-01-09 14:03:16 +0100

    singlenode

--- singlenode.sh
+++ singlenode.sh
@@ -23,8 +23,11 @@ then
     echo "killing previous instance of rmiregistry"
     kill -9 `ps -efj | tr -s " " | grep td366732 | grep rmiregistry | grep 1097 | cut -d' ' -f 2`
 fi
+
 echo "running registry on "`hostname`
 ./Registry.sh
+
+
 echo "starting tmux session on "`hostname`
 tmux new -s c -d
 tmux split-window -h -t c:0.0
