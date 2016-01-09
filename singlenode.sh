e77d8dc24e04f35ec40fe7eb863469b6a6f8d070
tomecki <tomek.dziopa@gmail.com>
2016-01-09 13:59:28 +0100

    rmiregistry restart

--- singlenode.sh
+++ singlenode.sh
@@ -1,15 +1,30 @@
 #!/usr/bin/env bash
-tmux kill-session -t c
-echo "running node from"`pwd`" on "`hostname`
-cd sr/cloudalbum
-nmap -p1097 localhost | grep open
 
-if [ $? -eq 1 ]
+echo "running node from"`pwd`" on "`hostname`
+
+
+# previous session cleanup
+tmux ls | grep c:
+if [ $? -eq 0 ]
 then
-    echo "running registry on "`hostname`
-    ./Registry.sh
+    tmux send-keys -t c:0.0 C-c C-m
+    tmux send-keys -t c:0.1 C-c C-m
+    sleep 1
+    tmux kill-session -t c
 fi
 
+cd sr/cloudalbum
+
+# checking for rmiregistry
+
+ps -efj | tr -s " " | grep td366732 | grep rmiregistry | grep 1097 | cut -d' ' -f 2
+if [ $? -eq 0 ]
+then
+    echo "killing previous instance of rmiregistry"
+    kill -9 `ps -efj | tr -s " " | grep td366732 | grep rmiregistry | grep 1097 | cut -d' ' -f 2`
+fi
+echo "running registry on "`hostname`
+./Registry.sh
 echo "starting tmux session on "`hostname`
 tmux new -s c -d
 tmux split-window -h -t c:0.0
