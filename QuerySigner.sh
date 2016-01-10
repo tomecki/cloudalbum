39542afe334e6d5590bbc9ef87c6f7bd12a7d229
tomecki <tomek.dziopa@gmail.com>
2016-01-10 21:03:34 +0100

    C-m

--- QuerySigner.sh
+++ QuerySigner.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 tmux new -s q -d
 tmux send-keys -t q:0.0 'cd sr/cloudalbum' C-m
-tmux send-keys -t q:0.0 'source Env.sh'
+tmux send-keys -t q:0.0 'source Env.sh' C-m
 tmux send-keys -t q:0.0 'CLOUDALBUMPATH="$BASEDIR/out/production/cloudalbum/:$BASEDIR/lib/objenesis-2.1.jar:$BASEDIR/lib/kryo-shaded-3.0.0.jar:$BASEDIR/lib/minlog-1.3.0.jar:$BASEDIR/lib/cup.jar:$BASEDIR/lib/JLex.jar"' C-m
 tmux send-keys -t q:0.0 '$JAVA_ENV -classpath $CLOUDALBUMPATH \
     $CLOUDALBUM_JAVA_OPTS \
