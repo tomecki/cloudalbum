f8f63d1ffe031a8826931096f75f0bf80ba77956
tomecki <tomek.dziopa@gmail.com>
2016-01-10 22:20:19 +0100

    kill old qs

--- QuerySigner.sh
+++ QuerySigner.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 cd sr/cloudalbum
+tmux ls | grep q:
+if [ $? -eq 0 ]
+then
+    tmux send-keys -t q C-c C-m
+    sleep 1
+    tmux kill-session -t q
+fi
 tmux new -s q -d
 tmux send-keys -t q 'source Env.sh' C-m
 tmux send-keys -t q 'CLOUDALBUMPATH="$BASEDIR/out/production/cloudalbum/:$BASEDIR/lib/objenesis-2.1.jar:$BASEDIR/lib/kryo-shaded-3.0.0.jar:$BASEDIR/lib/minlog-1.3.0.jar:$BASEDIR/lib/cup.jar:$BASEDIR/lib/JLex.jar"' C-m
