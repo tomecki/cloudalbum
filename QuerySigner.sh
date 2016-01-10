0940097094357627229b4350623717ddf02cddfa
tomecki <tomek.dziopa@gmail.com>
2016-01-10 20:58:53 +0100

    run qs

--- QuerySigner.sh
+++ QuerySigner.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-cd sr/cloudalbum
+tmux new -s q -d
+tmux send-keys -t q:0.0 'cd sr/cloudalbum
 source Env.sh
-CLOUDALBUMPATH="$BASEDIR/out/production/cloudalbum/:$BASEDIR/lib/objenesis-2.1.jar:$BASEDIR/lib/kryo-shaded-3.0.0.jar:$BASEDIR/lib/minlog-1.3.0.jar:$BASEDIR/lib/cup.jar:$BASEDIR/lib/JLex.jar"
-$JAVA_ENV -classpath $CLOUDALBUMPATH \
+CLOUDALBUMPATH="$BASEDIR/out/production/cloudalbum/:$BASEDIR/lib/objenesis-2.1.jar:$BASEDIR/lib/kryo-shaded-3.0.0.jar:$BASEDIR/lib/minlog-1.3.0.jar:$BASEDIR/lib/cup.jar:$BASEDIR/lib/JLex.jar"' C-m
+tmux send-keys -t q:0.0 '$JAVA_ENV -classpath $CLOUDALBUMPATH \
     $CLOUDALBUM_JAVA_OPTS \
     $CLOUDALBUM_DEBUG_OPTS \
     -Djava.rmi.server.hostname=localhost \
     -Djava.rmi.server.codebase=file:$BASEDIR/out/production/cloudalbum/ \
     -Djava.security.policy=FetcherServer.policy \
-	  pl.edu.mimuw.cloudalbum.querysigner.QuerySignerModule &
+	  pl.edu.mimuw.cloudalbum.querysigner.QuerySignerModule' C-m
