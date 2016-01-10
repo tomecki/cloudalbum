9f77f5a178fb040687dfe8e9b87f9d4e0eced950
tomecki <tomek.dziopa@gmail.com>
2016-01-10 20:40:21 +0100

    QS

---
+++ QuerySigner.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+source Env.sh
+CLOUDALBUMPATH="$BASEDIR/out/production/cloudalbum/:$BASEDIR/lib/objenesis-2.1.jar:$BASEDIR/lib/kryo-shaded-3.0.0.jar:$BASEDIR/lib/minlog-1.3.0.jar:$BASEDIR/lib/cup.jar:$BASEDIR/lib/JLex.jar"
+$JAVA_ENV -classpath $CLOUDALBUMPATH \
+    $CLOUDALBUM_JAVA_OPTS \
+    $CLOUDALBUM_DEBUG_OPTS \
+    -Djava.rmi.server.hostname=localhost \
+    -Djava.rmi.server.codebase=file:$BASEDIR/out/production/cloudalbum/ \
+    -Djava.security.policy=FetcherServer.policy \
+	  pl.edu.mimuw.cloudalbum.querysigner.QuerySignerModule
---
+++ src/pl/edu/mimuw/cloudalbum/tests/QuerySignerTest.java
@@ -0,0 +1,37 @@
+package pl.edu.mimuw.cloudalbum.tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+import pl.edu.mimuw.cloudalbum.agent.Agent;
+import pl.edu.mimuw.cloudalbum.eda.SignedEvent;
+import pl.edu.mimuw.cloudatlas.model.AttributesMap;
+import pl.edu.mimuw.cloudatlas.model.ZMI;
+
+import java.io.IOException;
+
+/**
+ * Created by tomek on 10.01.16.
+ */
+
+
+public class QuerySignerTest {
+
+    @Test
+    public void SignTest() throws Exception {
+        String path = "/a/b/c/d/e/f/g";
+        ZMI root = Agent.createZMIHierarchy(path);
+        String path2 = "/a/b/c/d/e/f/h";
+        ZMI root2 = Agent.createZMIHierarchy(path2);
+        SignedEvent<AttributesMap> am = new SignedEvent<>(root.getAttributes());
+        SignedEvent<AttributesMap> am2 = new SignedEvent<>(root2.getAttributes());
+        for(byte b: am.computeHash(am.getMessage())){
+            System.err.print(b);
+        } System.err.println();
+        for(byte b: am.getHash()){
+            System.err.print(b);
+        } System.err.println();
+        Assert.assertTrue(am.validate(root.getAttributes()));
+        Assert.assertFalse(am.validate(root2.getAttributes()));
+
+    }
+}
