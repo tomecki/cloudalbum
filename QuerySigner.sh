02162066ffa59e0833cc2246dfad25497547997b
tomecki <tomek.dziopa@gmail.com>
2016-01-10 21:10:13 +0100

    QS

--- QuerySigner.sh
+++ QuerySigner.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 cd sr/cloudalbum
 tmux new -s q -d
-tmux send-keys -t q:0.0 'source Env.sh' C-m
-tmux send-keys -t q:0.0 'CLOUDALBUMPATH="$BASEDIR/out/production/cloudalbum/:$BASEDIR/lib/objenesis-2.1.jar:$BASEDIR/lib/kryo-shaded-3.0.0.jar:$BASEDIR/lib/minlog-1.3.0.jar:$BASEDIR/lib/cup.jar:$BASEDIR/lib/JLex.jar"' C-m
-tmux send-keys -t q:0.0 '$JAVA_ENV -classpath $CLOUDALBUMPATH \
+tmux send-keys -t q 'source Env.sh' C-m
+tmux send-keys -t q 'CLOUDALBUMPATH="$BASEDIR/out/production/cloudalbum/:$BASEDIR/lib/objenesis-2.1.jar:$BASEDIR/lib/kryo-shaded-3.0.0.jar:$BASEDIR/lib/minlog-1.3.0.jar:$BASEDIR/lib/cup.jar:$BASEDIR/lib/JLex.jar"' C-m
+tmux send-keys -t q '$JAVA_ENV -classpath $CLOUDALBUMPATH \
     $CLOUDALBUM_JAVA_OPTS \
     $CLOUDALBUM_DEBUG_OPTS \
     -Djava.rmi.server.hostname=localhost \
