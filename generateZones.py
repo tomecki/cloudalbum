11faf0148d710dbecc9d42a2e670be5a25cda9c9
Tomasz Dziopa <td366732@students.mimuw.edu.pl>
2016-01-12 12:12:16 +0100

    Zone configuration: all nodes

--- generateZones.py
+++ generateZones.py
@@ -21,19 +21,22 @@ def main():
     querySignerHost = rooms[0][1][0]
     querySignerRoom = rooms[0][0]
 
+    f = open("selectedHosts", "wb")
+    totalHosts = []
+    for (room, cs) in rooms[:2]:
+        for c in cs[:3]:
+            f.write("/uw/mimuw/"+room+"/"+c+","+content[c]+"\n")
+            totalHosts += ["/uw/mimuw/"+room+"/"+c]
+    f.close()
 
     for (room, computers) in rooms[:2]:
         for c in computers[:3]:
             f = open("conf/"+c, "wb")
             f.write("contacts:0>"+",".join(filter(lambda x: x!= c, computers[:3]))+"#1>"+randomContacts(room, rooms[:2])+"\n")
             f.write("path:/uw/mimuw/"+room+"/"+c+"\n")
+            f.write("allNodes:"+",".join(totalHosts)+"\n")
             f.write("querySigner:"+'/uw/mimuw/'+querySignerRoom+'/'+querySignerHost+','+querySignerHost+"\n")
             f.write("agentDelay:5000\n")
             f.close()
-    f = open("selectedHosts", "wb")
-    for (room, cs) in rooms[:2]:
-        for c in cs[:3]:
-            f.write("/uw/mimuw/"+room+"/"+c+","+content[c]+"\n")
-    f.close()
 if __name__ == '__main__':
     main()
